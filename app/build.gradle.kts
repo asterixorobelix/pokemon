a83f154196904b13c929bc8e00b1d5077b745b86
Nathan <nathanstasin@zohomail.eu>
2024-03-16 13:07:21 +0200

    add retrofit and koin. network manifest permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.gson)
+    implementation(libs.koin)
+    implementation(libs.retrofit)
 
     testImplementation(libs.junit)
 
---
+++ app/src/main/java/asterixorobelix/pokemon/di/appModule.kt
@@ -0,0 +1,7 @@
+package asterixorobelix.pokemon.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
