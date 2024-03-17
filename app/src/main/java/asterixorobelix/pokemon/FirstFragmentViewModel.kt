1b63f4bdf133a296d06bfb76e019252ac927b8ec
Nathan <nathanstasin@zohomail.eu>
2024-03-17 19:27:25 +0200

    cleanup

--- app/src/main/java/asterixorobelix/pokemon/FirstFragmentViewModel.kt
+++ app/src/main/java/asterixorobelix/pokemon/FirstFragmentViewModel.kt
@@ -1,18 +1,13 @@
 package asterixorobelix.pokemon
 
-import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import asterixorobelix.pokemon.models.Pokemon
 import asterixorobelix.pokemon.models.Results
 import asterixorobelix.pokemon.repository.PokemonRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
 
 class FirstFragmentViewModel(private val pokemonRepository: PokemonRepository) : ViewModel() {
     val uiState: Flow<List<Results>> = flow {
